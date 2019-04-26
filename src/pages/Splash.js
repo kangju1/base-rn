45cda88293ca6da4aebfbd16de5a22b5ce9f2029
강주원 <kjw8384@hotmail.com>
2019-04-26 16:21:38 +0900

    Attach apisauce and moment

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text, SafeAreaView, Image} from 'react-native';
-
+import {create} from "apisauce";
+import Moment from 'moment';
 
 export default class Splash extends Component {
     constructor(props){
         super(props);
         this.state = {};
+        this.api = create({baseURL: 'https://facebook.com'});
+        this.api.get('ping').then(res=>{console.log(res)}).catch(err=>{console.log(err)})
     }
 
     render(){
